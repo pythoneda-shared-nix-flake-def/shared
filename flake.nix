77504356fd0c57bd574630962c9e6b1fd672116b
UnveilingPartner <github.bot@acm-sl.org>
2023-12-14 08:45:42 +0100

    Commit created with release-tag.sh

--- flake.nix
+++ flake.nix
@@ -28,19 +28,19 @@
         "pythoneda-shared-pythoneda-banner";
       inputs.pythoneda-shared-pythoneda-domain.follows =
         "pythoneda-shared-pythoneda-domain";
-      url = "github:pythoneda-shared-git-def/shared/0.0.16";
+      url = "github:pythoneda-shared-git-def/shared/0.0.17";
     };
     pythoneda-shared-pythoneda-banner = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
-      url = "github:pythoneda-shared-pythoneda-def/banner/0.0.36";
+      url = "github:pythoneda-shared-pythoneda-def/banner/0.0.37";
     };
     pythoneda-shared-pythoneda-domain = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythoneda-banner.follows =
         "pythoneda-shared-pythoneda-banner";
-      url = "github:pythoneda-shared-pythoneda-def/domain/0.0.11";
+      url = "github:pythoneda-shared-pythoneda-def/domain/0.0.12";
     };
     stringtemplate3 = {
       inputs.flake-utils.follows = "flake-utils";
