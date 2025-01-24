b7e622cecee0cbf057cadf08b3fdb82b20e05e74
Jose San Leandro Armendáriz <github@acm-sl.org>
2025-01-24 08:20:20 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-git-def/shared/0.0.93";
+      url = "github:pythoneda-shared-git-def/shared/0.0.94";
     };
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
@@ -40,7 +40,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.123";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.124";
     };
     stringtemplate3 = {
       inputs.flake-utils.follows = "flake-utils";
@@ -54,8 +54,8 @@
       let
         org = "pythoneda-shared-nix-flake";
         repo = "shared";
-        version = "0.0.20";
-        sha256 = "0f9xb6v499kqpggj92xmfhhxjk4gmg4f7343fk7hkq594hzyzc26";
+        version = "0.0.21";
+        sha256 = "0sr0d3q3yw08vv2k2q4n90fk6kh185bnk465rdv50i72dzn93n7z";
         pname = "${org}-${repo}";
         pythonpackage = "pythoneda.shared.nix.flake";
         package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
